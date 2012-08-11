38ba77073148583869553bd934918a11dd09387b
Chouri Soulaymen <sorakun@legtux.org>
2012-08-11 03:13:58 +0200

    New feature: Zoom
    Now you can change the view scale (zoom) of the preview area. (value
    shall be not 0).

--- iU.h
+++ iU.h
@@ -11,39 +11,39 @@ typedef char * string;
 
 typedef enum
 {
-  Add, Substract, Multiply, Devide,  Power, Sine, Cos, XVariable, Constant, Assignment
-}TNodeType;
+    Add, Substract, Multiply, Devide,  Power, Sine, Cos, XVariable, Constant, Assignment
+} TNodeType;
 
 typedef struct TOpType
 {
-  TNodeType op;
-  int priority; // The higher, the more prior.
-}TOpType;
+    TNodeType op;
+    int priority; // The higher, the more prior.
+} TOpType;
 
 typedef struct TNodePointer
 {
-  TNodeType node_type;
+    TNodeType node_type;
 
-  struct TNodePointer * LeftChild;
-  struct TNodePointer * RightChild;
- // struct TNodePointer * UnaryChild;
+    struct TNodePointer * LeftChild;
+    struct TNodePointer * RightChild;
+// struct TNodePointer * UnaryChild;
 
-  char varName[MAX_TOKEN_SIZE];
+    char varName[MAX_TOKEN_SIZE];
 
-  double ConstantData;
-}TNodePointer;
+    double ConstantData;
+} TNodePointer;
 
 typedef struct Table
 {
-  int size;
-  string * input;
-}Table;
+    int size;
+    string * input;
+} Table;
 
 typedef struct TVariable
 {
-  char name[MAX_TOKEN_SIZE];
-  double value;
-}TVariable;
+    char name[MAX_TOKEN_SIZE];
+    double value;
+} TVariable;
 
 #endif
 
